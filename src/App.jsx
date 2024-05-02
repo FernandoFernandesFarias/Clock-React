707673834139ff4714a5e7734e47d118bac11ea4
FernandoFernandesFarias <fariasfernando028@gmail.com>
2024-05-01 22:34:11 -0300

    Finalizando o estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Clock from './Clock'
 
 function App() {
 
   return (
     <div className="app">
-      
+      <Clock/>
     </div>
   )
 }
---
+++ src/clock.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+const Clock = () => {
+  return (
+    <div className="clock-container">
+      <div className="clock">
+        <h1>00:00:00</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Clock
